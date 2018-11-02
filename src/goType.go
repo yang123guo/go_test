ef1f3e7ccb1426e4e846a39232c013dcd0bd5619
yangguoqiang01 <wochange@163.com>
2018-11-03 01:15:48 +0800

    yang

--- src/goType.go
+++ src/goType.go
@@ -5,10 +5,25 @@ import (
 	"math"
 )
 
+type (
+	文本 string // 给string取一个别名 叫做 文本
+)
+
 func main() {
 	var a bool
 	var b string
+	var c byte
+	var d []byte
+	var e [1]bool     // 以上都是取默认值
+	var f 文本 = "我是中文" // 使用别名
+	g := "哈哈"         //  使用 := 和 不规定数据类型 自动推断
 	fmt.Println(math.MinInt8)
+	fmt.Println(math.MaxInt64)
 	fmt.Println(a)
-	fmt.Println(b)
+	fmt.Println(b, "**")
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Println(g)
 }
