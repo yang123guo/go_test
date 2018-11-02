84487614ae27df63fc5b6e1a3959a2a9fb598457
yangguoqiang01 <wochange@163.com>
2018-11-02 22:59:53 +0800

    go commit

---
+++ src/goBasic.go
@@ -0,0 +1,25 @@
+// 当前程序的包名称
+package main
+
+// 导入其他的包
+import "fmt"
+
+// 常量的定义
+const PI = 3.14
+
+// 全部变量的申明和赋值
+var name = "faith"
+
+// 一般类型申明
+type newType int
+
+// 结构的申明
+type gopher struct{}
+
+// 接口的申明
+type golang interface{}
+
+// 由main函数作为程序的入口点启动
+func main() {
+    fmt.Println("hello faith")
+}
